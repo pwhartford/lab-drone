7861736c2d4c24a47177144bb45ef55e1f9f756b
Peter Hartford <hartf055@umn.edu>
2024-08-19 17:24:06 +0200

    fix to keep sample frequency constant

--- arduino/wind_sensor_measurement/save_wind_sensor_data.py
+++ arduino/wind_sensor_measurement/save_wind_sensor_data.py
@@ -1,29 +1,38 @@
 from serial import Serial 
 from pathlib import Path 
 import numpy as np 
+from time import time 
+
 
 #SAVE TIME 
-N_SAMPLES = 10000
-DATA_PORT = '/dev/ttyACM0'
-BAUD_RATE = 115200
+N_SAMPLES = 1000
+DATA_PORT = '/dev/ttyACM1'
+BAUD_RATE = 57600
 
 #SAVE DIR 
 DATA_DIR = Path('/media/peter/share/Documents/Data/DroneData/HW_Sensor_Cal')
 
-DATA_FILE = DATA_DIR / 'hw_cal_01.csv'
+DATA_FILE = DATA_DIR / 'hw_cal_02.csv'
 
 serialConnection = Serial(DATA_PORT, BAUD_RATE)
 pressure = float(input('Enter the measured voltage: '))
 
+
 while True: 
-
     try:
-        data = np.zeros(N_SAMPLES)
-
-        for sample in range(N_SAMPLES):
-            data[sample] = serialConnection.readline()
+        # print(serialConnection.readline())
+        initialTime = time() 
+        array = np.frombuffer(serialConnection.readline(N_SAMPLES), dtype = 'uint8')
+        # print('Sample Frequency %0.2f Hz'%(1/(time()-intitialTime)), end = '\r')
+        print('Array Size: %s'%array.shape[0])
         
-        averageADCValue = np.nanmean(data)   
+        #Write handshake
+        if array.shape[0]<N_SAMPLES:
+            continue 
+        
+        print('Sample Frequency: %s'%(N_SAMPLES/(time()-initialTime)))
+
+        averageADCValue = np.mean(array)   
 
         with open(DATA_FILE, 'a') as file: 
             file.write('%f, %f'%(pressure, averageADCValue))
@@ -32,9 +41,13 @@ while True:
         print('Saved data avg: %s'%averageADCValue)
         pressure = float(input('Enter the measured voltage: '))
 
-    except: 
-        print('Read Failed... retrying collection')
-        continue 
+    except KeyboardInterrupt:
+        print('Stopping Acquisition')
+        break 
+
+    # except: 
+    #     print('Read Failed... retrying collection')
+    #     continue 
     
    
 
--- arduino/wind_sensor_measurement/wind_sensor_measurement.ino
+++ arduino/wind_sensor_measurement/wind_sensor_measurement.ino
@@ -1,19 +1,36 @@
+#define N_SAMPLES 1000
 
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
 
 const int OutPin  = A0;   // wind sensor analog pin  hooked up to Wind P sensor "OUT" pin
-const int TempPin = A2;   // temp sesnsor analog pin hooked up to Wind P sensor "TMP" pin
-const int SAMPLE_TIME = 10; //seconds 
-
+const int TempPin = A1;   // temp sesnsor analog pin hooked up to Wind P sensor "TMP" pin
+const unsigned long SAMPLE_FREQUENCY = 1000; //Hz 
+ 
+uint8_t data[N_SAMPLES];
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(57600);
 
 }
 
 void loop() {    
     // read wind pin 
-    int windAD = analogRead(OutPin);
-    Serial.println(windAD);
-  
+    for (int i=0; i<N_SAMPLES; i++){
+      currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+      
+      if (currentMillis - startMillis >= 1/SAMPLE_FREQUENCY*1000)  //test whether the period has elapsed
+      {
+          //read and print to serial 
+          data[i] = analogRead(OutPin);
+          // Serial.println(windAD);
+
+          startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
+      }
+    }
+    Serial.write((uint8_t*)data, (N_SAMPLES) * sizeof(uint8_t));
+    Serial.write('\n');
+    
+
 }
 
